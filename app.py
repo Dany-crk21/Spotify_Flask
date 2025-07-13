a2634e53e8a055c83d56b1e4536091bc9558f8b4
Daniel <danielandrada.ds@gmail.com>
2025-07-13 17:00:01 -0300

    modified app.py

--- app.py
+++ app.py
@@ -7,5 +7,14 @@ app = Flask (__name__)
 app.config["SQLALCHEMY_DATABASE_URI"]= DATABASE_CONNECTION_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+db.init_app(app)
+
+from Routes.canciones import Cancion_bp
+
+app.register_blueprint(Cancion_bp)
+
 if __name__ == '__main__':
     app.run(debug=True)
+    
+
+ 
